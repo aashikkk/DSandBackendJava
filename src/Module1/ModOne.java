9e35552b4b8a05e94cfea33c2134f0bf6c3cd0ad
aashikkk <sihaabama@gmail.com>
2024-11-06 13:45:21 +0530

    add

---
+++ src/Module1/ModOne.java
@@ -0,0 +1,19 @@
+package Module1;
+
+public class ModOne {
+    public static void main(String[] args) {
+        // Declaring an array
+//        int[] marks;
+//        marks = new int[5];
+        // OR
+//        int[] marks = new int[5];
+
+        // Initializing an array: (When you are allocating value - skip the size of the array)
+//        int[] marks = new int[] {10,20,30,40,50};
+
+        // Declare and assign
+
+
+    }
+}
+
---
+++ src/Module1/README.md
@@ -0,0 +1,17 @@
+# Arrays
+
+![img.png](imgs/Array.png)
+Same type - Homogeneous
+
+![img.png](imgs/AdvOfArrays.png)
+
+### Types
+
+1. Single Dimensional Array
+
+    Ex: Marks of 5 Students
+2. Multi Dimensional Array
+
+    Ex: Matrix
+
+![img.png](imgs/OneDimenArray.png)
